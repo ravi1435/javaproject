d11b4db49c23cc56b1d26aad0dede18bc9c55c24
ravi1435 <114794303+ravi1435@users.noreply.github.com>
2022-11-08 08:34:25 +0530

    Update App.java

--- src/main/java/javapro1/App.java
+++ src/main/java/javapro1/App.java
@@ -1,14 +1,27 @@
 package javapro1;
 
 public class App {
-  public static void main(String[] args) {
-      
-    // create an array
-    int[] numbers = {3, 7, 5, -5};
-    
-    // iterating through the array 
-    for (int number: numbers) {
-       System.out.println(number);
-    }
-  }
-}
+ public static void main(String[] args) {        
+             //Initialize array     
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};     
+         //Create another array arr2 with size of arr1    
+        int arr2[] = new int[arr1.length];    
+        //Copying all elements of one array into another    
+        for (int i = 0; i < arr1.length; i++) {     
+            arr2[i] = arr1[i];     
+        }      
+         //Displaying elements of array arr1     
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr1.length; i++) {     
+           System.out.print(arr1[i] + " ");    
+        }     
+            
+        System.out.println();    
+            
+        //Displaying elements of array arr2     
+        System.out.println("Elements of new array: ");    
+        for (int i = 0; i < arr2.length; i++) {     
+           System.out.print(arr2[i] + " ");    
+        }     
+    }    
+}    
